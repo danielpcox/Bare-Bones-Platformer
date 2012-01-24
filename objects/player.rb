dcfceaf7d1ea2f745b41ed471d73052508a99f62
Daniel Cox <danielpcox@gmail.com>
2012-01-24 15:33:51 -0500

    switched world dimensions back to large

--- objects/player.rb
+++ objects/player.rb
@@ -17,26 +17,17 @@ class Player
     w = @standing.width
     mass = h * w / 1000
     shape_array = [CP::Vec2.new(0,0), CP::Vec2.new(0, h), CP::Vec2.new(w, h), CP::Vec2.new(w, 0)]
-    #shape_array = [CP::Vec2.new(-hh, -hw), CP::Vec2.new(-hh, hw), CP::Vec2.new(hh, hw), CP::Vec2.new(hh, -hw)]
-    #CP::recenter_poly(shape_array)
-    #@body = CP::Body.new(mass, CP.moment_for_poly(mass, shape_array, CP::Vec2.new(0,0)))
     @body = CP::Body.new(mass, CP::INFINITY)
-    #@body = CP::Body.new_static()
     @body.p = CP::Vec2.new(x, y)
     @body.v_limit = PLAYER_MAX_V
-    #shape = CP::Shape::Poly.new(@body, shape_array, CP::Vec2.new(-w/2,-h))
     shape = CP::Shape::Circle.new(@body, 25.0, CP::Vec2.new(0.0,0.0))
     shape.u = 0.9 # friction coefficient
     shape.e = 0.0 # elasticity
-    #shape = CP::Shape::Poly.new(@body, shape_array, CP::Vec2.new(0,0))
     shape.collision_type = :player
 
     space.add_body(@body)
     space.add_shape(shape)
 
-    # This always points to the frame that is currently drawn.
-    # This is set in update, and used in draw.
-    @cur_image = @standing    
   end
 
   def go_left
@@ -89,16 +80,7 @@ class Player
   end
   
   def draw(camera)
-    # Flip vertically when facing to the left.
-    if @dir == :left then
-      offs_x = -25
-      factor = 1.0
-    else
-      offs_x = 25
-      factor = -1.0
-    end
-    #@cur_image.draw(*camera.world_to_screen(CP::Vec2.new(@body.p.x + offs_x, @body.p.y - 49)).to_a, 0, factor, 1.0)
-    @cur_image.draw_rot(*camera.world_to_screen(CP::Vec2.new(@body.p.x, @body.p.y)).to_a, ZOrder::Player, @body.a)
+    @standing.draw(*camera.world_to_screen(CP::Vec2.new(@body.p.x, @body.p.y)).to_a, ZOrder::Player)
   end
   
 end
