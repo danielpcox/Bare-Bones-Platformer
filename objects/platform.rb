2f762070a6d38de9fe69c0c716364976ccb02293
Daniel Cox <danielpcox@gmail.com>
2012-01-25 10:34:18 -0500

    cleaned up Platform class a bit

--- objects/platform.rb
+++ objects/platform.rb
@@ -2,26 +2,20 @@
 class Platform
   def initialize(window, x, y, image_filename = "dirtblocks.png")
     space = window.space
+    
+    # TODO : make my own platforms with a single image
     @tileset = Gosu::Image.load_tiles(window, "#{IMAGES_DIR}/#{image_filename}", 60, 60, true)
 
+    # create shape and body and add shape to space
     h = @tileset[0].height
     w = @tileset[0].width
-    mass = h * w
-    #shape_array = [CP::Vec2.new(-hh, -hw), CP::Vec2.new(-hh, hw), CP::Vec2.new(hh, hw), CP::Vec2.new(hh, -hw)]
     shape_array = [CP::Vec2.new(0,0), CP::Vec2.new(0, w), CP::Vec2.new(h, w), CP::Vec2.new(h, 0)]
-    #@body = CP::Body.new(mass, CP.moment_for_poly(mass, shape_array, CP::Vec2.new(0,0)))
     @body = CP::Body.new_static()
     @body.p = CP::Vec2.new(x, y)
     @body.object = self # set user-definable object field to be this Platform object
     shape = CP::Shape::Poly.new(@body, shape_array, CP::Vec2.new(0,0))
     shape.collision_type = :platform
-
-    #space.add_body(@body)
     space.add_shape(shape)
-  end
-
-  def add_to_world
-
   end
 
   def draw(camera)
